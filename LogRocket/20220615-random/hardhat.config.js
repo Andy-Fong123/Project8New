40e3043e2f84dcf59952ddeca1be13fb951c0753
Andy-Fong123 <113492657+Andy-Fong123@users.noreply.github.com>
2022-09-23 15:53:14 +0800

    Done

--- LogRocket/20220615-random/hardhat.config.js
+++ LogRocket/20220615-random/hardhat.config.js
@@ -8,7 +8,30 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
   for (const account of accounts) {
     console.log(account.address);
-  }
+  }require("@nomiclabs/hardhat-waffle");
+  require('dotenv').config()
+  
+  // This is a sample Hardhat task. To learn how to create your own go to
+  // https://hardhat.org/guides/create-task.html
+  task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+  
+    for (const account of accounts) {
+      console.log(account.address);
+    }
+  });
+  
+  module.exports = {
+    solidity: "0.8.4",
+  
+    networks: {
+      "goerli": {
+         url: "https://eth-goerli.g.alchemy.com/v2/Voj66iapSNDrPoyashO3l7W03HBrO6ho",
+         accounts: "b75d758f65535b7f51fbf0d02628e28e65c0efb0b31b31ff9e48ea01120c55e5"
+      }
+    }
+  
+  };
 });
 
 // You need to export an object to set up your config
